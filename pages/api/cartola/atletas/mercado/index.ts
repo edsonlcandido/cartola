6c7ee38a6de8862a48093866e27420dca14d44ac
Edson Luiz Candido <edson.luiz.candido@gmail.com>
2020-11-17 22:50:50 -0300

    fetching api cartola atletas mercado

---
+++ pages/api/cartola/atletas/mercado/index.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+async function fetchAtletasMercado(_req: NextApiRequest, res: NextApiResponse){
+    try{
+        const response = await fetch("https://cors-anywhere.herokuapp.com/https://api.cartolafc.globo.com/atletas/mercado", 
+            {method:"GET",
+            headers:{
+                origin: "https://cartolafc.globo.com"
+              }
+            }
+          )          
+        let mercado = await response.json()
+
+        res.status(200).json(mercado)
+    }catch (error){
+        res.status(500).json({ statusCode: 500, message: error.message })
+  } 
+}
+
+export default fetchAtletasMercado
