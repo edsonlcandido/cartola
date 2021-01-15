ae722ff88cd7825b231eb637bc1bd2e604575e1a
Edson Luiz Candido <edson.luiz.candido@gmail.com>
2021-01-14 22:42:18 -0300

    loading atletas from api cartola and showing in front end

--- components/Atletas.tsx
+++ components/Atletas.tsx
@@ -1,27 +1,42 @@
 import React, {Component, Fragment} from 'react'
 
+interface Atletas {
+  id: string,
+  title: string,
+  completed: boolean
+}
+
 class Mercado extends Component{
-    constructor(props: {} | Readonly<{}>){
-      super(props);
-  
-      this.state = {
-        data: null
-      };
-    }
-  
-    componentDidMount() {
-      fetch("/api/cartola/atletas/mercado")
-        .then(response => response.json())
-        .then(data => this.setState({ data }));
-    }
-  
-    render(){
-      return(
-        <Fragment>
-          Mercado
-        </Fragment>
-      )
-    }
+  state={
+    atletas:[],
+    loading: true,
+    error: false
   }
+
+/*   componentDidMount() {
+    fetch("/api/cartola/atletas/mercado")
+      .then(response => response.json())
+      .then(response => this.setState({atletas: response.atletas}));
+  } */
+  componentDidMount() {
+    fetch("/api/cartola/atletas/mercado")
+      .then(response => response.json())
+      .then(response => this.setState({
+        atletas: response.atletas,
+        loading:false
+      }));
+  }
+  render(){
+    const {atletas, loading, error} = this.state;
+    return(
+      <Fragment>
+        {loading && <div>Loading...</div>}
+        {!loading && atletas.map((atleta:any)=>
+          <li>{atleta.apelido}</li>
+        )}
+      </Fragment>
+    )
+  }
+}
 
   export {Mercado}
